7e38f8d682a4465ad71f12b473dc9c2c8725562a
Hjorthen <Hjorthensebastian@gmail.com>
2017-10-08 12:29:35 +0200

    Cleaned up registration code and hashed passwords

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -1,6 +1,8 @@
 'use strict';
 var mongoose = require('mongoose');
 var UserInfo = mongoose.model('User'); 
+var bcrypt = require('bcrypt');
+const saltRounds = 2;
 
 exports.signup_user = function(req, res){
 	if(!req.body) return res.sendStatus(400);
@@ -22,34 +24,42 @@ exports.signup_user = function(req, res){
 		res.end(JSON.stringify(error));
 		return;
 	}
-	
 		
-	UserInfo.count({_id : req.body.username}, function(err, count) {
-			if(count > 0){
-				const error = { error : "A user is already registered with that id"};
+	//Register user.
+	let registerUser = function(saltedPassword){
+	var user = new UserInfo({ _id : req.body.username, Firstname : "somebody", Lastname : "lastname", phone_number_1 : 0, role : "guest", password : saltedPassword });	
+	user.save(function(err, _) {
+		if(err) {
+			if(err.code == 11000){ // Duplicate key
+				const error = { error : "A user with that name already exists" };
 				res.status(400).send(JSON.stringify(error));
-				return;
 			}
 			else {
-									
-					//TODO: Verify CAS 	
-					//Register user.
-					var user = new UserInfo({ _id : req.body.username, Firstname : "somebody", Lastname : "lastname", phone_number_1 : 0, role : "guest", password : req.body.password });	
-					user.save(function(err, _) {
-						if(err) {
-							console.error(err);
-							const error = { error : "Failed to save user"};
-							res.status(500).send(JSON.stringify(error));
-						}
-						else {
-							res.status(200);
-							res.end();
-							
-						}
-					});
-				}
-
+					console.error(err);
+					const error = { error : "Failed to save user"};
+					res.status(500).send(JSON.stringify(error));
+			}
+		}
+		else {
+			res.status(200);
+			res.end();
+			
+		}
+	});
+	};
+			
+	//TODO: Verify CAS 	
+	bcrypt.hash(req.body.password, saltRounds, function(err, salt){
+		if(err){
+			const error = { error : "Failed to hash password" };
+			res.status(500).send(JSON.stringify(error));
+			return;
+		}
+		else{
+			registerUser(salt);	
+		}
 	});	
+
 };
 
 
