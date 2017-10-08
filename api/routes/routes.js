74460dedff3e8a737746ac73b69492ef7baa98d5
mrdanielpc <danielplaetner@gmail.com>
2017-10-08 11:13:48 +0200

    created project controller, route and tested them

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,7 +1,16 @@
 'use strict';
 module.exports = function(app) {
-  var controller = require('../controllers/projectController');
+  var project = require('../controllers/projectController');
 
-  // todoList Routes
-  app.route('/project')
+
+ 	// project Routes
+ 	app.route('/project')
+    	.get(project.list_all_projects)
+    	.post(project.create_project);
+
+	app.route('/project/:projectId')
+	 	.get(project.read_a_project)
+	 	.put(project.update_a_project)
+	 	.delete(project.delete_a_project);
+
 };
