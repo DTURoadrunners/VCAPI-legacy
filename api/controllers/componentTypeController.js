da558a21d8c857947028909e882ce5e81f0b36e7
mrdanielpc <danielplaetner@gmail.com>
2017-10-23 12:11:28 +0200

    you can create componentType

--- api/controllers/componentTypeController.js
+++ api/controllers/componentTypeController.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose'),
     ComponentType = mongoose.model('ComponentType'),
     Log = mongoose.model('Log'),
-    Project = mongoose.model('Project');
+    Project = mongoose.model('Project'),
+    logController = require('./logController');
 
 exports.list_all_componentsTypes = function (req, res) {
     Project.findById(req.params.projectId, function (err, project) {
@@ -20,6 +21,42 @@ exports.list_all_componentsTypes = function (req, res) {
 
 
 exports.create_a_componentType = function(req, res) {
+    
+    var new_componentType = new ComponentType(req.body);
+    var username = "ThomasTemp"; //TODO: Get user id from JWT
+    var submitionComment = req.body.comment;
+    new_componentType.validate(function (err) {
+        if (err) {
+            console.log("error 1");
+            res.status(500).send(err);
+        } else {
+            //Create component type
+            new_componentType.save(function(err, model) {
+                if(err) {
+                    console.log("error 2");
+                    res.status(500).send(err);
+                } else {
+                    //Create new reference in project
+                    Project.findByIdAndUpdate(
+                        req.params.projectId,
+                        { $push: { "component_type": new_componentType} },
+                        { safe: true, upsert: false, new: true },
+                        function (err, model) {
+                            if (err) {
+                                res.status(500).send(err);
+                            }
+                            else {
+                                //Create log entry
+                                logController.create_log(model._id, username, submitionComment, "Created", new_componentType._id, res);
+                            }
+                        }
+                    );
+                }
+            } )
+        }
+    });
+
+    /*
     var new_componentType = new ComponentType(req.body);
     var submitionComment = req.body.comment;
     var username = "ThomasTemp"; //TODO: Get user id from JWT
@@ -45,7 +82,7 @@ exports.create_a_componentType = function(req, res) {
             );
         }
     });
-
+    */
 };
 
 
