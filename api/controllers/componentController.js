9f305d69c0adc60206a43d5391494b27079bb1ab
Thomas Bech Madsen <thomasbm@live.dk>
2017-10-29 11:01:31 +0100

    Implemented delete. Does not work, same problem as update

--- api/controllers/componentController.js
+++ api/controllers/componentController.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-  Component = mongoose.model('Component'),
-  Project = mongoose.model('Project'),
-  ComponentType = mongoose.model('ComponentType'),
-  Log = mongoose.model('Log'),
-  ComponentLog = mongoose.model('ComponentLog'),
-  logController = require('./logController');
+    Component = mongoose.model('Component'),
+    Project = mongoose.model('Project'),
+    ComponentType = mongoose.model('ComponentType'),
+    Log = mongoose.model('Log'),
+    ComponentLog = mongoose.model('ComponentLog'),
+    logController = require('./logController');
 
 exports.list_all_components = function (req, res) {
     ComponentType.find({ "_id": req.params.componentTypeId })
@@ -25,19 +25,19 @@ exports.list_all_components = function (req, res) {
 
 
 
-exports.create_a_component = function(req, res) {
+exports.create_a_component = function (req, res) {
     var new_component = new Component(req.body);
     var username = "ThomasTemp"; //TODO: Get user id from JWT
     var submitionComment = "commentComment"; //req.body.logComment;
-    
-    if(!submitionComment){
-        res.json(400, { error : "No logComment supplied" });
+
+    if (!submitionComment) {
+        res.json(400, { error: "No logComment supplied" });
         return;
     }
 
-    new_component.validate(function(err) {
-    if (err) 
-        res.status(500).send(err);  
+    new_component.validate(function (err) {
+        if (err)
+            res.status(500).send(err);
         return;
     });
 
@@ -66,18 +66,18 @@ exports.create_a_component = function(req, res) {
 };
 
 
-exports.read_a_component = function(req, res) {
-    
+exports.read_a_component = function (req, res) {
+
     Component.findById(
         req.params.componentId,
         function (err, model) {
-        if (err) {
-            res.status(500).send(err);
-        }
-        else {
-            res.json(model);
-        }
-    })
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.json(model);
+            }
+        })
 
 };
 
@@ -86,7 +86,7 @@ exports.update_a_component = function (req, res) {
     var new_component = new Component(req.body);
     var username = "ThomasTemp"; //TODO: Get user id from JWT
     var submitionComment = "commentComment"; //req.body.logComment;
-    
+
     if (!submitionComment) {
         res.json(400, { error: "No logComment supplied" });
         return;
@@ -106,16 +106,13 @@ exports.update_a_component = function (req, res) {
             //Switch reference to new component
             ComponentType.findByIdAndUpdate(
                 { "_id": req.params.componentTypeId, "component": req.params.componentId },
-                { $set: { "component.$" : new_component } },
+                { $set: { "component.$": new_component } },
                 { safe: true, upsert: false, new: true },
                 function (err, model) {
                     if (err) {
                         res.status(500).send(err);
                     }
                     else {
-                        //Create log entry
-                        console.log(new_component);
-                        console.log(model);
                         logController.create_log(model._id, username, submitionComment, "Updated", req.params.componentId, res);
                     }
                 }
@@ -125,12 +122,31 @@ exports.update_a_component = function (req, res) {
 };
 
 
-exports.delete_a_component = function(req, res) {
-  Component.remove({
-    _id: req.params.componentId
-  }, function(err, component) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Component successfully deleted' });
-  });
+exports.delete_a_component = function (req, res) {
+    var username = "ThomasTemp"; //TODO: Get user id from JWT
+    var submitionComment = "commentComment"; //req.body.logComment;
+
+    if (!submitionComment) {
+        res.json(400, { error: "No logComment supplied" });
+        return;
+    }
+
+    new_component.validate(function (err) {
+        if (err)
+            res.status(500).send(err);
+        return;
+    });
+    //Delete reference to component
+    ComponentType.findByIdAndRemove( //Not tested, unlikely to work. Same problem as update?
+        { "_id": req.params.componentTypeId, "component": req.params.componentId },
+        { safe: true, upsert: false, new: true },
+        function (err, model) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                logController.create_log(model._id, username, submitionComment, "Updated", req.params.componentId, res);
+            }
+        }
+    );
 };
